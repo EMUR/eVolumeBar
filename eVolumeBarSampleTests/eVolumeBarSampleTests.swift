161fced017f4259483c0494033975919fb9fe273
E <eyad@eyads-macbook-pro.local>
2018-01-09 00:06:22 -0800

    Initial Commit

---
+++ eVolumeBarSampleTests/eVolumeBarSampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  eVolumeBarSampleTests.swift
+//  eVolumeBarSampleTests
+//
+//  Created by Eyad on 1/9/18.
+//  Copyright © 2018 Eyad. All rights reserved.
+//
+
+import XCTest
+@testable import eVolumeBarSample
+
+class eVolumeBarSampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
