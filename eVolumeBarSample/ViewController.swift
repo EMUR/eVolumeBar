dc3917c44b4ce24b05579149bac78539db810855
E <eyad@eyads-macbook-pro.local>
2018-01-09 00:49:14 -0800

    add sample project files

--- eVolumeBarSample/ViewController.swift
+++ eVolumeBarSample/ViewController.swift
@@ -9,17 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var volume:eVolumeBar!
+    
+    @IBAction func enableDefault(_ sender: UIButton) {
+        view.viewWithTag(111)?.removeFromSuperview()
+        
+        volume = eVolumeBar()
+        self.view.addSubview(volume)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func enableDefaultCIC(_ sender: UIButton) {
+        view.viewWithTag(111)?.removeFromSuperview()
+        
+        volume = eVolumeBar().withVolumeIconColor(#colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1))
+        self.view.addSubview(volume)
     }
-
-
+    
+    @IBAction func enableDefaultWT(_ sender: UIButton) {
+        view.viewWithTag(111)?.removeFromSuperview()
+        
+        volume = eVolumeBar().withTint(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1))
+        self.view.addSubview(volume)
+    }
+    
+    @IBAction func enableDefaultCI(_ sender: UIButton) {
+        view.viewWithTag(111)?.removeFromSuperview()
+        
+        volume = eVolumeBar(withImageName: "space")
+        self.view.addSubview(volume)
+    }
+    
+    @IBAction func enableDefaultCIV(_ sender: UIButton) {
+        view.viewWithTag(111)?.removeFromSuperview()
+        
+        volume = eVolumeBar(withImages: ["mute","min","max"], forVolumes: [0.0,0.5,1.0])
+        self.view.addSubview(volume)
+    }
+    
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
 }
 
